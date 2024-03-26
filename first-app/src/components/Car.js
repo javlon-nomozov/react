c8bb2bd19d1c8c7d8f92bbaf036fd04e2d43b19b
Joe McNeil <69708014+samarqandiy@users.noreply.github.com>
2024-03-27 01:24:41 +0500

    end

---
+++ first-app/src/components/Car.js
@@ -0,0 +1,6 @@
+// components and file names must start with uppercase letters
+function Car() {
+  return <h2>Hi, I am a Car!</h2>;
+}
+
+export default Car;
--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// importing Car Component
+import Car from "./components/Car"
+
+// ---------------------------------------
+//  using variables in react
+// const name = "Javlon";
+
+// const myFirstElement = (
+//   <>
+//     <h1>React is {5 + 5} times better with JSX</h1>
+//     {/* <h1>The learner name is {name}</h1> */}
+//   </>
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(myFirstElement);
+
+
+// ---------------------------------------
+// components with Props
+
+function Garage() {
+  return (
+    <>
+      <h1>Who lives in my Garage?</h1>
+      <Car />
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Garage />);
